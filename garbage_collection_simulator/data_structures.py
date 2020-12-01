64200bf510ccc538384ba0dcb3228850ebc255e8
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2020-12-01 21:04:41 +0330

    Added Stack implementation needed in General List printing Added General List expression to list conversion

--- garbage_collection_simulator/data_structures.py
+++ garbage_collection_simulator/data_structures.py
@@ -1,83 +1,116 @@
-class NotEnoughMemoryCellsError(MemoryError):
+class NotEnoughMemoryNodesError(MemoryError):
+    pass
+
+
+class StackEmptyError(ValueError):
     pass
 
 
 class Memory:
     def __init__(self, size):
         """
-        A memory simulator instance is created with given number of cells.
-        Each cell occupies 4 consequent indices with names: tag, label, next, down.
-        Thus cells array have 4 * size indices.
+        A memory simulator instance is created with given number of nodes.
+        Each node occupies 4 consequent indices with names: tag, label, next, down.
+        Thus nodes array have 4 * size indices.
         """
-        self.__cells = [None] * (size * 4)
+        self.__nodes = [None] * (size * 4)
         self.__size = size
         self.__avail_list_head = 0
         self.__init_avail_list()
 
     def __init_avail_list(self):
         for i in range(0, 4 * (self.__size - 1), 4):
-            self.set_cell_next(i, i + 4)
+            self.set_node_next(i, i + 4)
 
-    def allocate_cell(self):
+    def allocate_node(self, new_node_label = None):
         if self.__avail_list_head is None:
-            raise NotEnoughMemoryCellsError("Can not allocate cells due to insufficient memory space.")
-        next_avail_cell = self.get_cell_next(self.__avail_list_head)
-        allocated_cell = self.__avail_list_head
-        self.set_cell_next(allocated_cell, None)
-        self.__avail_list_head = next_avail_cell
-        return allocated_cell
+            raise NotEnoughMemoryNodesError("Can not allocate nodes due to insufficient memory space.")
+        next_avail_node = self.get_node_next(self.__avail_list_head)
+        allocated_node = self.__avail_list_head
+        self.set_node_next(allocated_node, None)  # sets next pointer to None
+        self.set_node_down(allocated_node, None)  # sets down pointer to None
+        self.set_node_label(allocated_node, new_node_label)
+        self.__avail_list_head = next_avail_node
+        return allocated_node
 
-    def free_cell(self, cell):
-        self.set_cell_next(cell, self.__avail_list_head)
-        self.__avail_list_head = cell
+    def free_node(self, node):
+        self.set_node_next(node, self.__avail_list_head)
+        self.__avail_list_head = node
 
-    def set_cell_next(self, cell, new_next):
-        self.__cells[cell + 2] = new_next
+    def set_node_next(self, node, new_next):
+        self.__nodes[node + 2] = new_next
 
-    def set_cell_down(self, cell, new_down):
-        self.__cells[cell + 3] = new_down
+    def set_node_down(self, node, new_down):
+        self.__nodes[node + 3] = new_down
 
-    def set_cell_label(self, cell, new_label):
-        self.__cells[cell + 1] = new_label
+    def set_node_label(self, node, new_label):
+        self.__nodes[node + 1] = new_label
 
-    def get_cell_label(self, cell):
-        return self.__cells[cell + 1]
+    def get_node_label(self, node):
+        return self.__nodes[node + 1]
 
-    def get_cell_down(self, cell):
-        return self.__cells[cell + 3]
+    def get_node_down(self, node):
+        return self.__nodes[node + 3]
 
-    def get_cell_next(self, cell):
-        return self.__cells[cell + 2]
+    def get_node_next(self, node):
+        return self.__nodes[node + 2]
 
-    def __get_cell_tag(self, cell):
-        return self.__cells[cell]
+    def __get_node_tag(self, node):
+        return self.__nodes[node]
 
-    def __set_cell_tag(self, cell, new_tag):
-        self.__cells[cell] = new_tag
+    def __set_node_tag(self, node, new_tag):
+        self.__nodes[node] = new_tag
 
     def status(self):
         return [
             (
-                self.__cells[4 * i],  # tag
-                self.__cells[4 * i + 1],  # label
-                self.__cells[4 * i + 2],  # next
-                self.__cells[4 * i + 3]  # down
+                self.__nodes[4 * i],  # tag
+                self.__nodes[4 * i + 1],  # label
+                self.__nodes[4 * i + 2],  # next
+                self.__nodes[4 * i + 3]  # down
             ) for i in range(self.__size)
         ]
 
     def __str__(self):
         return "\n".join(
             [
-                f"cell #{i + 1} : "
-                f"(tag = {self.__cells[4 * i]}, "
-                f"label = {self.__cells[4 * i + 1]}, "
-                f"next = {self.__cells[4 * i + 2]}, "
-                f"down = {self.__cells[4 * i + 3]})" for i in range(self.__size)
+                f"node #{i + 1} : "
+                f"(tag = {self.__nodes[4 * i]}, "
+                f"label = {self.__nodes[4 * i + 1]}, "
+                f"next = {self.__nodes[4 * i + 2]}, "
+                f"down = {self.__nodes[4 * i + 3]})" for i in range(self.__size)
             ]
         )
 
     def garbage_collect(self, *lists_roots):
         pass
+
+
+class Stack:
+    def __init__(self, memory: Memory):
+        self.__top = None
+        self.__memory = memory
+
+    def top(self):
+        if self.is_empty():
+            raise StackEmptyError("Stack is empty.")
+        return self.__memory.get_node_label(self.__top)
+
+    def is_empty(self):
+        return self.__top is None
+
+    def push(self, element):
+        new_node = self.__memory.allocate_node()
+        self.__memory.set_node_label(new_node, element)
+        self.__memory.set_node_down(new_node, self.__top)
+        self.__top = new_node
+
+    def pop(self):
+        top_element = self.__memory.get_node_label(self.__top)
+        top_node = self.__top
+        self.__top = self.__memory.get_node_down(self.__top)
+        self.__memory.free_node(top_node)
+        return top_element
 
 
 class GeneralList:
@@ -87,19 +120,41 @@ class GeneralList:
 
     @classmethod
     def convert_expression_to_general_list(cls, memory: Memory, expression: str):
+        ptr_to_ptr, _ = cls.__convert_expression_to_general_list(memory, list(expression), 0)
+        ptr = memory.get_node_down(ptr_to_ptr)
+        memory.free_node(ptr_to_ptr)
         return cls(
             memory,
-            cls.__convert_expression_to_general_list(memory, list(expression), 0)
+            ptr
         )
 
     @staticmethod
     def __convert_expression_to_general_list(memory: Memory, expression: list, idx):
-        pass
-
+        node = memory.allocate_node()
+        previous_node = node
+        while idx < len(expression):
+            if expression[idx] == "(":
+                new_node = memory.allocate_node()
+                down_node, idx = GeneralList.__convert_expression_to_general_list(memory, expression, idx + 1)
+                memory.set_node_down(new_node, down_node)
+                memory.set_node_next(previous_node, new_node)
+                previous_node = new_node
+            elif expression[idx] == ")":
+                return node, idx + 1
+            else:
+                new_node = memory.allocate_node()
+                memory.set_node_label(new_node, expression[idx])
+                memory.set_node_next(previous_node, new_node)
+                previous_node = new_node
+                idx += 1
+        return node, idx
 
     def find_node_by_expression(self, node_expression):
         pass
 
     def __str__(self):
-        pass
+        return ""
+        #result = ["("]
+        #stack = Stack(self.__memory)  # used to store pointers to nodes
+        #p = self.__root
 
--- tests/data_structures_test.py
+++ tests/data_structures_test.py
@@ -1,5 +1,6 @@
 import unittest
-from garbage_collection_simulator.data_structures import Memory, NotEnoughMemoryCellsError
+from garbage_collection_simulator.data_structures import Memory, NotEnoughMemoryNodesError, \
+    GeneralList, Stack, StackEmptyError
 
 
 class MemoryTest(unittest.TestCase):
@@ -15,12 +16,12 @@ class MemoryTest(unittest.TestCase):
             ]
         )
 
-    def test_allocate_cell(self):
+    def test_allocate_node(self):
         memory = Memory(4)
-        cell1 = memory.allocate_cell()
-        memory.set_cell_label(cell1, "A")
-        cell2 = memory.allocate_cell()
-        memory.set_cell_label(cell2, "B")
+        node1 = memory.allocate_node()
+        memory.set_node_label(node1, "A")
+        node2 = memory.allocate_node()
+        memory.set_node_label(node2, "B")
         self.assertEqual(
             memory.status(),
             [
@@ -33,15 +34,15 @@ class MemoryTest(unittest.TestCase):
 
     def test_free_node(self):
         memory = Memory(4)
-        cell_a = memory.allocate_cell()
-        memory.set_cell_label(cell_a, "A")
-        cell_b = memory.allocate_cell()
-        memory.set_cell_label(cell_b, "B")
-        memory.free_cell(cell_a)
-        memory.set_cell_label(memory.allocate_cell(), "C")
-        memory.set_cell_label(memory.allocate_cell(), "D")
-        memory.free_cell(cell_b)
-        memory.set_cell_label(memory.allocate_cell(), "E")
+        node_a = memory.allocate_node()
+        memory.set_node_label(node_a, "A")
+        node_b = memory.allocate_node()
+        memory.set_node_label(node_b, "B")
+        memory.free_node(node_a)
+        memory.set_node_label(memory.allocate_node(), "C")
+        memory.set_node_label(memory.allocate_node(), "D")
+        memory.free_node(node_b)
+        memory.set_node_label(memory.allocate_node(), "E")
         self.assertEqual(
             memory.status(),
             [
@@ -52,11 +53,46 @@ class MemoryTest(unittest.TestCase):
             ]
         )
 
-    def test_allocate_cell_raising_error(self):
+    def test_allocate_node_raising_error(self):
         memory = Memory(5)
         for _ in range(5):
-            memory.allocate_cell()
-        self.assertRaises(NotEnoughMemoryCellsError, memory.allocate_cell)
+            memory.allocate_node()
+        self.assertRaises(NotEnoughMemoryNodesError, memory.allocate_node)
+
+
+class StackTest(unittest.TestCase):
+    def test_push(self):
+        mem = Memory(5)
+        stack = Stack(mem)
+        stack.push("A")
+        stack.push("B")
+        stack.push("C")
+        self.assertEqual("C", stack.top())
+
+    def test_pop(self):
+        mem = Memory(5)
+        stack = Stack(mem)
+        stack.push("A")
+        stack.push("B")
+        stack.push("C")
+        stack.pop()
+        self.assertEqual("B", stack.top())
+
+    def test_top_raising_error(self):
+        mem = Memory(5)
+        stack = Stack(mem)
+        stack.push("A")
+        stack.push("B")
+        stack.pop()
+        stack.pop()
+        self.assertRaises(StackEmptyError, stack.top)
+
+
+class GeneralListTest(unittest.TestCase):
+    def test_convert_expression_to_general_list(self):
+        memory = Memory(30)
+        result_list = GeneralList.convert_expression_to_general_list(memory, "(a(b(c)d)ef(g)((i)h))")
+        self.assertEqual(str(result_list), "(a(b(c)d)ef(g)((i)h))")
 
 
 if __name__ == '__main__':
