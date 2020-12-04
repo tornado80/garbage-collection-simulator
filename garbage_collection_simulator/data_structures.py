aeb904954ff5008482eea90d689c054caf4da16c
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2020-12-04 03:31:26 +0330

    Implemented well-tested general list printing and expression conversion

--- garbage_collection_simulator/data_structures.py
+++ garbage_collection_simulator/data_structures.py
@@ -22,15 +22,15 @@ class Memory:
         for i in range(0, 4 * (self.__size - 1), 4):
             self.set_node_next(i, i + 4)
 
-    def allocate_node(self, new_node_label = None):
+    def allocate_node(self, new_label = None, new_down = None, new_next = None):
         if self.__avail_list_head is None:
             raise NotEnoughMemoryNodesError("Can not allocate nodes due to insufficient memory space.")
         next_avail_node = self.get_node_next(self.__avail_list_head)
         allocated_node = self.__avail_list_head
-        self.set_node_next(allocated_node, None)  # sets next pointer to None
-        self.set_node_down(allocated_node, None)  # sets down pointer to None
-        self.set_node_label(allocated_node, new_node_label)
         self.__avail_list_head = next_avail_node
+        self.set_node_next(allocated_node, new_next)
+        self.set_node_down(allocated_node, new_down)
+        self.set_node_label(allocated_node, new_label)
         return allocated_node
 
     def free_node(self, node):
@@ -82,7 +82,7 @@ class Memory:
             ]
         )
 
-    def garbage_collect(self, *lists_roots):
+    def garbage_collect(self, *roots_list):
         pass
 
 
@@ -100,9 +100,7 @@ class Stack:
         return self.__top is None
 
     def push(self, element):
-        new_node = self.__memory.allocate_node()
-        self.__memory.set_node_label(new_node, element)
-        self.__memory.set_node_down(new_node, self.__top)
+        new_node = self.__memory.allocate_node(new_label = element, new_down = self.__top)
         self.__top = new_node
 
     def pop(self):
@@ -121,40 +119,59 @@ class GeneralList:
     @classmethod
     def convert_expression_to_general_list(cls, memory: Memory, expression: str):
         ptr_to_ptr, _ = cls.__convert_expression_to_general_list(memory, list(expression), 0)
-        ptr = memory.get_node_down(ptr_to_ptr)
-        memory.free_node(ptr_to_ptr)
-        return cls(
-            memory,
-            ptr
-        )
+        return cls(memory, ptr_to_ptr)
 
     @staticmethod
     def __convert_expression_to_general_list(memory: Memory, expression: list, idx):
-        node = memory.allocate_node()
-        previous_node = node
+        first_node = None
+        previous_node = first_node
         while idx < len(expression):
             if expression[idx] == "(":
                 new_node = memory.allocate_node()
+                if previous_node is not None:
+                    memory.set_node_next(previous_node, new_node)
+                else:
+                    first_node = new_node
+                previous_node = new_node
                 down_node, idx = GeneralList.__convert_expression_to_general_list(memory, expression, idx + 1)
                 memory.set_node_down(new_node, down_node)
-                memory.set_node_next(previous_node, new_node)
-                previous_node = new_node
             elif expression[idx] == ")":
-                return node, idx + 1
+                return first_node, idx + 1
             else:
-                new_node = memory.allocate_node()
-                memory.set_node_label(new_node, expression[idx])
-                memory.set_node_next(previous_node, new_node)
+                new_node = memory.allocate_node(new_label = expression[idx])
+                if previous_node is not None:
+                    memory.set_node_next(previous_node, new_node)
+                else:
+                    first_node = new_node
                 previous_node = new_node
                 idx += 1
-        return node, idx
+        return first_node, idx
 
     def find_node_by_expression(self, node_expression):
         pass
 
     def __str__(self):
-        return ""
-        #result = ["("]
-        #stack = Stack(self.__memory)  # used to store pointers to nodes
-        #p = self.__root
-
+        result = []
+        stack = Stack(self.__memory)
+        stack.push(self.__root)
+        reached_end = False
+        while not stack.is_empty():
+            top = stack.top()
+            if reached_end:
+                stack.pop()
+                result.append(")")
+                if self.__memory.get_node_next(top) is not None:
+                    reached_end = False
+                    stack.push(self.__memory.get_node_next(top))
+                continue
+            if self.__memory.get_node_down(top) is None:
+                result.append(self.__memory.get_node_label(top))
+                stack.pop()
+                if self.__memory.get_node_next(top) is None:
+                    reached_end = True
+                else:
+                    stack.push(self.__memory.get_node_next(top))
+            else:
+                result.append("(")
+                stack.push(self.__memory.get_node_down(top))
+        return "".join(result)
